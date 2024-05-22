84a0d36ca26addbd45498053beb6f373e09b53cb
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2024-05-22 18:40:16 +0100

    modified

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from typing import Literal 
 
 
 app = Flask(__name__)
@@ -23,7 +22,7 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> tuple[str, Literal[401]]:
+def unauthorized(error) -> str:
     """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
