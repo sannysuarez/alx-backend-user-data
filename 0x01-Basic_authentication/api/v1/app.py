23088d9db1a0d8ecea70f2042826091e383c3078
sanny suarez <68450026+sannysuarez@users.noreply.github.com>
2024-05-22 18:29:59 +0100

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,7 +23,7 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Not found handler
+    """ unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
