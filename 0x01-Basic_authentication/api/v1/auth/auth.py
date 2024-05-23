38e417facd54835f58daaa24958d601386419f43
alfadaniel <alfadaniel1991@gmail.com>
2024-05-23 15:06:54 +0100

    fixed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,42 @@
+#!/usr/bin/env python3
+from flask import request
+'''
+Model to manage the API authentication
+'''
+
+class Auth:
+    '''
+    manage the API authentication
+    '''
+
+def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+    '''
+    '''
+    return False
+
+def authorization_header(self, request=None) -> str:
+    '''
+    '''
+    return
+
+def current_user(self, request=None) -> typeVar('User'):
+    '''
+    '''
+    return
+
+if __name__=='__main__':
+    a = Auth()
+
+    print(a.require_auth("/api/v1/status/", ["/api/v1/status"]))
+    print(a.authorization_header())
+    print(a.current_user())
+
+
+
+
+
+
+
 
   
   
