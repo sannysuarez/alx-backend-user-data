4cbb79c611a6346663fecbabd49f198865921081
alfadaniel <alfadaniel1991@gmail.com>
2024-05-23 15:31:13 +0100

    fixed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,29 +8,32 @@ class Auth:
     '''
     manage the API authentication
     '''
+    def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
+        '''
+         check if authentication is required to access path
+        '''
+        return False
+    
+    
+    def authorization_header(self, request=None) -> str:
+        '''
+        '''
+        return None
+    
+ 
+    def current_user(self, request=None) -> typeVar('User'):
+        '''
+        '''
+        return None
+    
 
-def require_auth(self, path: str, excluded_paths: list[str]) -> bool:
-    '''
-    check if authentication is required to access path
-    '''
-    return False
 
-def authorization_header(self, request=None) -> str:
-    '''
-    '''
-    return
 
-def current_user(self, request=None) -> typeVar('User'):
-    '''
-    '''
-    return
 
-if __name__=='__main__':
-    a = Auth()
 
-    print(a.require_auth("/api/v1/status/", ["/api/v1/status"]))
-    print(a.authorization_header())
-    print(a.current_user())
+
+
+
 
 
 
