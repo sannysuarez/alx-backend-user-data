1ab922253d058f9ab32a0101627c97a0fd91ea74
alfadaniel <alfadaniel1991@gmail.com>
2024-05-23 18:45:38 +0100

    fixed

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,9 +2,8 @@
 from flask import request
 from typing import List, TypeVar
 '''
-Model to manage the API authentication
+Model to manage the API authenticatio
 '''
-
 class Auth:
     '''
     manage the API authentication
@@ -13,6 +12,20 @@ class Auth:
         '''
         check if authentication is required to access path
         '''
+         # be slash tolerant: path=/api/v1/status and path=/api/v1/status/
+        if path and not path.endwith('/'):
+            path = path + '/'   
+        # Return True if path is known
+        if not path or path not in excluded_paths:
+            return True
+        # Return True if excluded_path is None or empty
+        if not excluded_paths or excluded_paths == []:
+            return True
+
+        # Return False if path is in excluded_paths
+        if path in excluded_paths:
+            return False
+        # You can assume excluded_paths contains string path alway sending by a /
         return False
     
 
@@ -22,7 +35,7 @@ class Auth:
         return
     
 
-    def current_user(self, request=None) -> typeVar('User'):
+    def current_user(self, request=None) -> TypeVar('User'):
         '''
         '''
         return
